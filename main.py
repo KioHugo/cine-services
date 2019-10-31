2727d81f5ccc31ed426c67454820c92d7bf1d42a
KioHugo <caillaud.hugo.44@gmail.com>
2019-10-31 16:40:40 +0100

    Séparation serie main

--- main.py
+++ main.py
@@ -1,36 +1,21 @@
 from flask import Flask
 from flask_restplus import Resource, Api
-import mysql.connector
+import Serie as serie_func
 
 app = Flask(__name__)
 api = Api(app)
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="root",
-    database="projet-cine"
-)
-mycursor = mydb.cursor()
 
-SELECT_ALL_SERIES = "SELECT * FROM serie"
-
-
-@api.route('/series', '/serie')
+@api.route('/series')
 class Series(Resource):
     def get(self):
-        mycursor.execute(SELECT_ALL_SERIES)
-        les_series = mycursor.fetchall()
-        series_json = {"series": []}
-        for series in les_series:
-            la_serie = {
-                "id": str(series[0]),
-                "nom": str(series[1].decode()),
-                "description": str(series[2].decode())
-            }
-            series_json['series'].append(la_serie)
+        return serie_func.Series(Resource).get()
 
-        return series_json
+
+@api.route('/serie/<id>')
+class Serie(Resource):
+    def get(self, id):
+        return serie_func.Serie(Resource).get(id)
 
 
 if __name__ == '__main__':
