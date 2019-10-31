0552b5e9acfb3589fe405b33f96f89bbb841036d
KioHugo <caillaud.hugo.44@gmail.com>
2019-10-31 14:59:24 +0100

    - add main

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restplus import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+@api.route('/series')
+class HelloWorld(Resource):
+    def get(self):
+        return {'series': [
+            {
+                'nom': 'daybreak'
+            }
+        ]}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
