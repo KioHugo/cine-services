4d2120ce0eabbaa205a05c471d4c6e9e773e8bac
KioHugo <31588247+KioHugo@users.noreply.github.com>
2020-01-13 20:24:37 +0100

    - Ajout des namespace pour la doc swagger - Modif script

--- main.py
+++ main.py
@@ -11,10 +11,14 @@ api = Api(app)
 
 CORS(app, resources={r"/*": {"origins": "*"}})
 
+serie_ns = api.namespace('Serie', description='Actions sur les series')
+film_ns = api.namespace('Film', description='Actions sur les films')
+acteur_ns = api.namespace('Acteur', description='Actions sur les acteurs')
 
-@api.route('/series')
+@serie_ns.route('/series')
 class Series(Resource):
     def get(self):
+        ''' Affiche toutes les séries '''
         data = getAllSeries()
         if data == False:
             api.abort(404)
@@ -22,9 +26,10 @@ class Series(Resource):
             return data
 
 
-@api.route('/serie/<id>')
+@serie_ns.route('/serie/<id>')
 class SerieById(Resource):
     def get(self, id):
+        ''' Affiche un série en fonction de son id '''
         data = getSerieById(id)
         if data == False:
             api.abort(404)
@@ -32,9 +37,10 @@ class SerieById(Resource):
             return data
 
 
-@api.route('/films')
+@film_ns.route('/films')
 class Film(Resource):
     def get(self):
+        ''' Affiche tous les films '''
         data = getAllFilms()
         if data == False:
             api.abort(404)
@@ -42,9 +48,10 @@ class Film(Resource):
             return data
 
 
-@api.route('/film/<id>')
+@film_ns.route('/film/<id>')
 class FilmById(Resource):
     def get(self, id):
+        ''' Affiche un film en fonction de son id '''
         data = getFilmById(id)
         if data == False:
             api.abort(404)
@@ -52,9 +59,10 @@ class FilmById(Resource):
             return data
 
 
-@api.route('/acteurs')
+@acteur_ns.route('/acteurs')
 class Acteur(Resource):
     def get(self):
+        ''' Affiche tous les acteurs '''
         data = getAllActeur()
         if data == False:
             api.abort(404)
@@ -62,9 +70,10 @@ class Acteur(Resource):
             return data
 
 
-@api.route('/acteur/<id>')
+@acteur_ns.route('/acteur/<id>')
 class ActeurById(Resource):
     def get(self, id):
+        ''' Affiche un acteur en fonction de son id '''
         data = getActeurById(id)
         if data == False:
             api.abort(404)
@@ -74,4 +83,4 @@ class ActeurById(Resource):
 
 if __name__ == '__main__':
     # app.run(debug=True, host='192.168.43.140')
-    app.run(debug=True, threaded=True)
+    app.run(debug=True, threaded=True, port=472)
